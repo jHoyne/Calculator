37c2e57ec745d281a008be394e60377361148f7b
Joseph Hoyne <JosephHoyne@gmail.com>
2015-06-30 05:25:38 -0500

    Added Everything Created thus far

---
+++ src/scripts/basic.js
@@ -0,0 +1,315 @@
+//Global Variables
+var count = 0;
+// Adds one
+function add_one() {
+    var total = count + 1;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added One(1)");
+    console.log(count);
+}
+// Subtracts One
+function sub_one() {
+    var total = count - 1;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Removed One(1)");
+    console.log(count);
+}
+// Adds Two
+function add_two() {
+    var total = count + 2;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added Two(2)");
+    console.log(count);
+}
+// Subtract Two
+function sub_two() {
+    var total = count - 2;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted Two(2)");
+    console.log(count);
+}
+// Multiply by Two
+function multiply_two() {
+    var total = count * 2;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Multiplied by Two(2)");
+    console.log(count);
+}
+// Divides by Two
+function divide_two() {
+    var total = count / 2;
+    document.getElementById('final').innerHTML = count;
+    if (count !== count);
+    count = total;
+    console.log("Divided by Two(2)");
+    console.log(count);
+}
+// Adds Three
+function add_three() {
+    var total = count + 3;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added Three(3)");
+    console.log(count);
+}
+// Subtract Three
+function sub_three() {
+    var total = count - 3;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted Three(3)");
+    console.log(count);
+}
+// Multiply by Three
+function multiply_three() {
+    var total = count * 3;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Multiplied by Three(3)");
+    console.log(count);
+}
+// Divides by Three
+function divide_three() {
+    var total = count / 3;
+    document.getElementById('final').innerHTML = count;
+    if (count !== count);
+    count = total;
+    console.log("Divided by Three(3)");
+    console.log(count);
+}
+// Adds Four
+function add_four() {
+    var total = count + 4;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added Four(4)");
+    console.log(count);
+}
+// SUbtracts Four
+function sub_four() {
+    var total = count - 4;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted Four(4)");
+    console.log(count);
+}
+// Multiply Four
+function multiply_four() {
+    var total = count * 4;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Multiplied by Four(4)");
+    console.log(count);
+}
+// Divide by Four
+function divide_four() {
+    var total = count / 4;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Divided by Four(4)");
+    console.log(count);
+}
+// Adds five
+function add_five() {
+    var total = count + 5;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added Five(5)");
+    console.log(count);
+}
+// Subtracts 5
+function sub_five() {
+    var total = count - 5;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted Five(5)");
+    console.log(count);
+}
+// Multiply 5
+function multiply_five() {
+    var total = count * 5;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Multiplied by Five(5)");
+    console.log(count);
+}
+// Divide 5
+function divide_five() {
+    var total = count / 5;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Divided by Five(5)");
+    console.log(count);
+}
+// Adds 10
+function add_ten() {
+    var total = count + 10;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added Ten(10)");
+    console.log(count);
+}
+// Subtracts 10
+function sub_ten() {
+    var total = count - 10;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted Ten(10)");
+    console.log(count);
+}
+// Multiplies by 10
+function multiply_ten(){
+    var total = count * 10;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Multiplied by Ten");
+    console.log(count);
+}
+// Divide 10
+function divide_ten(){
+    var total = count / 10;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Divided by Ten(10)");
+    console.log(count);
+}
+// Adds 50
+function add_fifty() {
+    var total = count + 50;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added Fifty(50)");
+    console.log(count);
+}
+// Subtracts 50
+function sub_fifty() {
+    var total = count - 50;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted Fifty(50)");
+    console.log(count);
+}
+// Adds 100
+function add_hundred() {
+    var total = count + 100;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added One Hundred(100)");
+    console.log(count);
+}
+// Subtracts 500
+function sub_hundred() {
+    var total = count - 100;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted One Hundred(100)")
+    console.log(count);
+}
+// Adds 500
+function add_five_hundred() {
+    var total = count + 500;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added Five Hundred(500)");
+    console.log(count);
+}
+//Subtract 500
+function sub_five_hundred() {
+    var total = count - 500;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted Five Hundred(500)");
+    console.log(count);
+}
+// Add 1000
+function add_thousand() {
+    var total = count + 1000;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added One Thousand(1000)");
+    console.log(count);
+}
+// Subtract 1000
+function sub_thousand() {
+    var total = count - 1000;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted One Thousand(1000)");
+    console.log(count);
+}
+//Clears Value of add_to
+function reset() {
+    count = 0;
+    document.getElementById('final').innerHTML = count;
+    if (count !== 0);
+    count = count;
+    console.log("Cleared Count");
+    console.log(count);
+}
+// Add Custom
+function custom_add(){
+    alert("This is not possible currently I will find a workaround.\nIt is not possible because of the '+' being adding the var rather than the numbers behind the var");
+}
+// Subtract custom Number
+function custom_sub(){
+    var custom = prompt("Enter A Number to be Subtracted.");
+    var total = count - custom;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log(custom + " Was Subtracted");
+    console.log(count);
+}
+// Multiplies Custom Number
+function custom_multiply(){
+    var custom = prompt("Enter a Number to Multiply the count by");
+    var total = count * custom;
+    document.getElementById('final').innerHTML = total;
+    if(count !== count);
+    count = total;
+    console.log(custom + " has multiplied the Count");
+    console.log(count);
+}
+// Divides Custom Number
+function custom_divide(){
+    var custom = prompt("Enter a Number to Divide the count by");
+    var total = count / custom;
+    document.getElementById('final').innerHTML = total;
+    if(count !== count);
+    count = total;
+    console.log(custom + " has divided the count");
+    console.log(count);
+}
