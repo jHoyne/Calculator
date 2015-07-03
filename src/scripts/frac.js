072e20cd7fa38a14f1d4f59ca2fa8580769cd997
Joseph Hoyne <JosephHoyne@gmail.com>
2015-07-03 05:25:48 -0500

    Finalized frac.js for current time

--- src/scripts/frac.js
+++ src/scripts/frac.js
@@ -4,15 +4,24 @@ var oneSixth = 1 / 6; // 0.16
 var oneFive = 1 / 5; // 0.2
 var oneFour = 1 / 4; // 0.25
 var oneThree = 1 / 3; // 0.33
+var threeEight = 3 / 8; //* 0.375
+var twoFive = 2 / 5; //* 0.4
+var half = 1 / 2; //* 0.5
+var threeFive = 3 / 5; //* 0.6
+var fiveEight = 5 / 8; //* 0.625
+var twoThree = 2 / 3; //* 0.66
+var threeFour = 3 / 4; //* 0.75
+var fourFive = 4 / 5; //* 0.8
+var fiveSix = 5 / 6; //* 0.83
+var sevenEight = 7 / 8; //* 0.875
 // Addition
-// 
 // 
 function add_oneEight() {
     var total = count + oneEight;
     document.getElementById('final').innerHTML = total;
     if (count !== count);
     count = total;
-    console.log("Added One Eighth(1/8)");
+    console.log("Added One Eighth(0.125)");
     console.log(count);
 }
 
@@ -21,7 +30,7 @@ function add_oneSix() {
     document.getElementById('final').innerHTML = total;
     if (count !== count);
     count = total;
-    console.log("Added One Sixth(1/6)");
+    console.log("Added One Sixth(0.16)");
     console.log(count);
 }
 
@@ -30,7 +39,7 @@ function add_oneFive() {
     document.getElementById('final').innerHTML = total;
     if (count !== count);
     count = total;
-    console.log("Added One Fith(1/5)");
+    console.log("Added One Fith(0.2)");
     console.log(count);
 }
 
@@ -39,7 +48,7 @@ function add_oneFour() {
     document.getElementById('final').innerHTML = total;
     if (count !== count);
     count = total;
-    console.log("Added One Fourth(1/4)");
+    console.log("Added One Fourth(0.25)");
     console.log(count);
 }
 
@@ -48,18 +57,107 @@ function add_oneThree() {
     document.getElementById('final').innerHTML = total;
     if (count !== count);
     count = total;
-    console.log("Added One Third(1/3)");
+    console.log("Added One Third(0.33)");
+    console.log(count);
+}
+
+function add_threeEight() {
+    var total = count + threeEight;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added Three Eighth(0.375)");
+    console.log(count);
+}
+
+function add_twoFive() {
+    var total = count + twoFive;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added Two Fiths(0.4)");
+    console.log(count);
+}
+
+function add_oneHalf() {
+    var total = count + half;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added One Half(0.5)");
+    console.log(count);
+}
+
+function add_threeFive() {
+    var total = count + threeFive;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added Three Fiths(0.6)");
+    console.log(count);
+}
+
+function add_fiveEight() {
+    var total = count + fiveEight;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added Five Eigthts(0.625)");
+    console.log(count);
+}
+
+function add_twoThree() {
+    var total = count + twoThree;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added Two Thirds(0.63)");
+    console.log(count);
+}
+
+function add_threeFour() {
+    var total = count + threeFour;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added Three Fourths(0.75)");
+    console.log(count);
+}
+
+function add_fourFive() {
+    var total = count + fourFive;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added Four Fiths(0.8)");
+    console.log(count);
+}
+
+function add_fiveSix() {
+    var total = count + fiveSix;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added Five Sixth(0.83)");
+    console.log(count);
+}
+
+function add_sevenEight() {
+    var total = count + sevenEight;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added Seven Eigthts(0.875)");
     console.log(count);
 }
 // Subtraction
-// 
 // 
 function sub_oneEight() {
     var total = count - oneEight;
     document.getElementById('final').innerHTML = total;
     if (count !== count);
     count = total;
-    console.log("Subtracted One Eighth(1/8)");
+    console.log("Subtracted One Eighth(0.125)");
     console.log(count);
 }
 
@@ -68,7 +166,7 @@ function sub_oneSix() {
     document.getElementById('final').innerHTML = total;
     if (count !== count);
     count = total;
-    console.log("Subtracted One Sixth(1/6)");
+    console.log("Subtracted One Sixth(0.16)");
     console.log(count);
 }
 
@@ -77,7 +175,7 @@ function sub_oneFive() {
     document.getElementById('final').innerHTML = total;
     if (count !== count);
     count = total;
-    console.log("Subtracted One Fith(1/5)");
+    console.log("Subtracted One Fith(0.2)");
     console.log(count);
 }
 
@@ -86,7 +184,7 @@ function sub_oneFour() {
     document.getElementById('final').innerHTML = total;
     if (count !== count);
     count = total;
-    console.log("Subtracted One Fourth(1/4)");
+    console.log("Subtracted One Fourth(0.25)");
     console.log(count);
 }
 
@@ -95,6 +193,96 @@ function sub_oneThree() {
     document.getElementById('final').innerHTML = total;
     if (count !== count);
     count = total;
-    console.log("Subtracted One Third(1/3)");
+    console.log("Subtracted One Third(0.33)");
+    console.log(count);
+}
+
+function sub_threeEight() {
+    var total = count - threeEight;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted Three Eighth(0.375)");
+    console.log(count);
+}
+
+function sub_twoFive() {
+    var total = count - twoFive;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted Two Fiths(2/5)");
+    console.log(count);
+}
+
+function sub_oneHalf() {
+    var total = count - half;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted One Half(0.5)");
+    console.log(count);
+}
+
+function sub_threeFive() {
+    var total = count - threeFive;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted Three Fiths(0.6)");
+    console.log(count);
+}
+
+function sub_fiveEight() {
+    var total = count - fiveEight;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted Five Eighths(0.625)");
+    console.log(count);
+}
+
+function sub_twoThree() {
+    var total = count - twoThree;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted Two Thirds(0.33)");
+    console.log(count);
+}
+
+function sub_threeFour() {
+    var total = count - threeFour;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted Three Fourths(0.75)");
+    console.log(count);
+}
+
+function sub_fourFive() {
+    var total = count - fourFive;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted Four Fiths(0.8)");
+    console.log(count);
+}
+
+function sub_fiveSix() {
+    var total = count - fiveSix;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted Five Sixths(0.83)");
+    console.log(count);
+}
+
+function sub_sevenEight() {
+    var total = count - sevenEight;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted Seven Eigthts(0.875)");
     console.log(count);
 }
