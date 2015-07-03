a5685206a5e4166a3d14f9fc659770ed3517c4d3
Joseph Hoyne <JosephHoyne@gmail.com>
2015-07-03 03:31:37 -0500

    Added 4 more fraction, I tired

--- src/scripts/frac.js
+++ src/scripts/frac.js
@@ -1,6 +1,9 @@
 //Fractoral Variables
 var oneEight = 1 / 8; // 0.125
 var oneSixth = 1 / 6; // 0.16
+var oneFive = 1 / 5; // 0.2
+var oneFour = 1 / 4; // 0.25
+var oneThree = 1 / 3; // 0.33
 // Addition
 // 
 // 
@@ -21,6 +24,33 @@ function add_oneSix() {
     console.log("Added One Sixth(1/6)");
     console.log(count);
 }
+
+function add_oneFive() {
+    var total = count + oneFive;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added One Fith(1/5)");
+    console.log(count);
+}
+
+function add_oneFour() {
+    var total = count + oneFour;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added One Fourth(1/4)");
+    console.log(count);
+}
+
+function add_oneThree() {
+    var total = count + oneThree;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Added One Third(1/3)");
+    console.log(count);
+}
 // Subtraction
 // 
 // 
@@ -39,5 +69,32 @@ function sub_oneSix() {
     if (count !== count);
     count = total;
     console.log("Subtracted One Sixth(1/6)");
+    console.log(count);
+}
+
+function sub_oneFive() {
+    var total = count - oneFive;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted One Fith(1/5)");
+    console.log(count);
+}
+
+function sub_oneFour() {
+    var total = count - oneFour;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted One Fourth(1/4)");
+    console.log(count);
+}
+
+function sub_oneThree() {
+    var total = count - oneThree;
+    document.getElementById('final').innerHTML = total;
+    if (count !== count);
+    count = total;
+    console.log("Subtracted One Third(1/3)");
     console.log(count);
 }
