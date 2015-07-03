7e676426a9428cda1c138fdd1362b804f49f542a
Joseph Hoyne <JosephHoyne@gmail.com>
2015-07-02 23:35:43 -0500

    frac + OO = possible errors please result to using version one as this is a test version.

---
+++ src/scripts/frac.js
@@ -0,0 +1,9 @@
+function add_oneEight(){
+    var oneEight = 1 / 8;
+    var total = count + oneEight;
+    document.getElementById('final').innerHTML = total;
+    if(count !== count);
+    count = total;
+    console.log("Added One Eighth");
+    console.log(count);
+}
