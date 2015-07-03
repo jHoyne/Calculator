f9100d3bc02ed4051ca22bd0c1fb6ede147122fa
Joseph Hoyne <JosephHoyne@gmail.com>
2015-07-03 01:07:13 -0500

    fractions pls wrk

--- src/scripts/frac.js
+++ src/scripts/frac.js
@@ -1,9 +1,41 @@
+//Fractoral Variables
+var oneEight = 1 / 8;  // 0.125
+var oneSixth = 1 / 6;  // 0.16
+// Addition
+// 
+// 
 function add_oneEight(){
-    var oneEight = 1 / 8;
     var total = count + oneEight;
     document.getElementById('final').innerHTML = total;
     if(count !== count);
     count = total;
-    console.log("Added One Eighth");
+    console.log("Added One Eighth(1/8)");
+    console.log(count);
+}
+function add_oneSix(){
+    var total = count + oneSixth;
+    document.getElementById('final').innerHTML = total;
+    if(count !== count);
+    count = total;
+    console.log("Added One Sixth(1/6)");
+    console.log(count);
+}
+// Subtraction
+// 
+// 
+function sub_oneEight(){
+    var total = count - oneEight;
+    document.getElementById('final').innerHTML = total;
+    if(count !== count);
+    count = total;
+    console.log("Subtracted One Eighth(1/8)");
+    console.log(count);
+} 
+function sub_oneSix(){
+    var total = count - oneSixth;
+    document.getElementById('final').innerHTML = total;
+    if(count !== count);
+    count = total;
+    console.log("Subtracted One Sixth(1/6)");
     console.log(count);
 }
