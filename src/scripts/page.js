4729680c356446c5ab819470455c5bad4a0d7242
Joseph Hoyne <JosephHoyne@gmail.com>
2015-07-04 20:35:19 -0500

    Added a Footer

--- src/scripts/page.js
+++ src/scripts/page.js
@@ -1,5 +1,10 @@
 //Global Variables
 var count = 0;
+// Thanks
+thanks()
+function thanks(){
+    alert('Thank you for using my software I did not expect pay for this code, If you run into any issues/suggestions they can be reported at https://github.com/jHoyne/calculator');
+}
 //Clears Value of 'final'
 function reset() {
     count = 0;
