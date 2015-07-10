292c8e1e922f82eb3a937a34f01e85a4c5d31f68
Joseph Hoyne <JosephHoyne@gmail.com>
2015-07-09 20:56:41 -0500

    Toggle added to div allowment

--- src/scripts/page.js
+++ src/scripts/page.js
@@ -19,3 +19,11 @@ function EnableDevMode(){
    
     docHead.removeChild (script);
 }
+// Toggle Basic
+function toggle_visibility(id) {
+   var element = document.getElementById(id);
+   if(element.style.display == 'block')
+      element.style.display = 'none';
+   else
+      element.style.display = 'block';
+}
