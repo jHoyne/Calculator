17027db601d855d77fb55f586bccc54323dea9d1
Joseph Hoyne <JosephHoyne@gmail.com>
2015-08-29 12:40:12 -0500

    Hallo

--- src/scripts/clock.js
+++ src/scripts/clock.js
@@ -1,5 +1,5 @@
 function date_timer() {
     var curDate = new Date();
     document.getElementById('clockCT').innerHTML = curDate;
-    setTimeout(date_timer(), 1000);
+    setInterval(date_timer(), 1000);
 }
